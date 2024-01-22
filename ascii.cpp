23a746c447286f490341a25c024b0ee82febdd69
Meet Raval <101318831+Meetr8870@users.noreply.github.com>
2024-01-23 01:40:39 +0530

    Add files via upload

---
+++ ascii.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cout<<"enter the char";
+    cin>> c;
+    cout<< "the ascII value of  chaar  "<<c<< " is "<<int(c);
+    return 0;
+}
